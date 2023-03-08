99cba73eb8744038176a061050a96cb0c9a5143e
Yinchen Ni <84832528+hamham223@users.noreply.github.com>
2023-03-08 16:16:07 +0800

    Update conftest.py

--- python/orca/test/bigdl/orca/learn/ray/tf/conftest.py
+++ python/orca/test/bigdl/orca/learn/ray/tf/conftest.py
@@ -22,7 +22,7 @@ from pyspark.sql import SparkSession
 @pytest.fixture(autouse=True, scope='package')
 def orca_context_fixture():
     conf = {"spark.python.worker.reuse": "false"}
-    sc = init_orca_context(cores=8)
+    sc = init_orca_context(init_ray_on_spark=False)
 
     def to_array_(v):
         return v.toArray().tolist()
