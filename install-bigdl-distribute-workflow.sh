007010b50337781e4b43979e8d2b87555a6dabea
Hamster <niyinchen@sjtu.edu.cn>
2023-04-23 16:57:51 +0800

    ci: version 3 kub

--- install-bigdl-distribute-workflow.sh
+++
@@ -1,6 +0,0 @@
-# Configure the variables to be passed into the templates.
-export http_proxy=your_http_proxy
-export https_proxy=your_https_proxy
-export no_proxy=your_no_proxy
-
-envsubst < ./bigdl-distribute-workflow.yaml | kubectl create -f - -n your-namespace
